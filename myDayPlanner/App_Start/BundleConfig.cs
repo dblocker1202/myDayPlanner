4b1e3ef9730fb4eb53a6784c7d94e9dff88be0b8
David Blocker <dblocker5899@gmail.com>
2018-07-23 20:52:56 -0500

    adding bundle config

---
+++ myDayPlanner/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Optimization;
+namespace myDayPlanner.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts/desktop")
+                       .Include(
+                            "~/Scripts/jquery-{version}.js",
+                            "~/Scripts/jquery-ui-{version}.js")
+                       .IncludeDirectory(
+                            "~/Scripts", "*.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css/desktop")
+                       .Include(
+                           "~/Content/bootstrap.css")
+                        .IncludeDirectory("~/Content", "*.css"));
+        }
+    }
+}
--- myDayPlanner/Global.asax.cs
+++ myDayPlanner/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using myDayPlanner.App_Start;
 
 namespace myDayPlanner
 {
@@ -10,6 +12,7 @@ namespace myDayPlanner
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
